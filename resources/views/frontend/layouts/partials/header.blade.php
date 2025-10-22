cf3022a6e4a8b3ef7c0f861e12270949ed58fac1
Wasif Ahmed <155315416+wasifahmed85@users.noreply.github.com>
2025-10-22 16:23:02 +0600

    Update header.blade.php

--- resources/views/frontend/layouts/partials/header.blade.php
+++ resources/views/frontend/layouts/partials/header.blade.php
@@ -2,7 +2,7 @@
     <div class="bg-zinc-500 py-4">
         <div class="container mx-auto flex items-center justify-between px-4">
             <a href="{{ route('home') }}" wire:navigate>
-                <h1 class="text-2xl font-bold text-white">B33 Rentals</h1>
+                <h1 class="text-2xl font-bold text-white">Kivor</h1>
             </a>
             <nav class="flex items-center justify-end gap-4">
                 @auth
