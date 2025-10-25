9baec6d893a656deb6c8f236cb65064e8f535abc
Wasif Ahmed <155315416+wasifahmed85@users.noreply.github.com>
2025-10-25 11:11:28 +0600

    application settings

---
+++ app/Models/ApplicationSetting.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\BaseModel;
+
+class ApplicationSetting extends BaseModel
+{
+    //
+
+    protected $fillable = [
+        'sort_order',
+        'key',
+        'env_key',
+        
+        'created_by',
+        'updated_by',
+        'deleted_by',
+    ];
+
+    protected $hidden = [
+        //
+    ];
+
+    protected $casts = [
+        //
+    ];
+
+    /* =#=#=#=#=#=#=#=#=#=#==#=#=#=#= =#=#=#=#=#=#=#=#=#=#==#=#=#=#=
+                Start of RELATIONSHIPS
+     =#=#=#=#=#=#=#=#=#=#==#=#=#=#= =#=#=#=#=#=#=#=#=#=#==#=#=#=#= */
+
+     //
+
+     /* =#=#=#=#=#=#=#=#=#=#==#=#=#=#= =#=#=#=#=#=#=#=#=#=#==#=#=#=#=
+                End of RELATIONSHIPS
+     =#=#=#=#=#=#=#=#=#=#==#=#=#=#= =#=#=#=#=#=#=#=#=#=#==#=#=#=#= */
+
+    public function __construct(array $attributes = [])
+    {
+        parent::__construct($attributes);
+        $this->appends = array_merge(parent::getAppends(), [
+            //
+        ]);
+    }
+
+
+}
---
+++ database/migrations/2025_10_25_045121_create_application_settings_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use App\Traits\AuditColumnsTrait;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    use AuditColumnsTrait;
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('application_settings', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('sort_order')->default(0);
+            $table->string('key', 255)->unique();
+            $table->string('env_key')->nullable();
+
+            $table->softDeletes();
+            $table->timestamps();
+
+            $this->addAdminAuditColumns($table);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('application_settings');
+    }
+};
