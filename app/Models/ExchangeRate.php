74ce9f5ad5da9c2cc2f980f693a773623fa6addd
Wasif Ahmed <155315416+wasifahmed85@users.noreply.github.com>
2025-10-25 10:34:53 +0600

    ExchangeRate done

---
+++ app/Models/ExchangeRate.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\BaseModel;
+
+class ExchangeRate extends BaseModel
+{
+    //
+
+    protected $fillable = [
+        'sort_order',
+         'base_currency',
+        'target_currency',
+        'rate',
+        'last_updated_at',
+
+
+        'created_by',
+        'updated_by',
+        'deleted_by',
+
+      //here AuditColumns 
+    ];
+
+    protected $hidden = [
+        //
+    ];
+
+    protected $casts = [
+         'last_updated_at' => 'datetime',
+    ];
+
+    /* =#=#=#=#=#=#=#=#=#=#==#=#=#=#= =#=#=#=#=#=#=#=#=#=#==#=#=#=#=
+                Start of RELATIONSHIPS
+     =#=#=#=#=#=#=#=#=#=#==#=#=#=#= =#=#=#=#=#=#=#=#=#=#==#=#=#=#= */
+
+    public function base()
+    {
+        return $this->belongsTo(Currency::class, 'base_currency', 'id');
+    }
+
+    public function target()
+    {
+        return $this->belongsTo(Currency::class, 'target_currency', 'id');
+    }
+
+     /* =#=#=#=#=#=#=#=#=#=#==#=#=#=#= =#=#=#=#=#=#=#=#=#=#==#=#=#=#=
+                End of RELATIONSHIPS
+     =#=#=#=#=#=#=#=#=#=#==#=#=#=#= =#=#=#=#=#=#=#=#=#=#==#=#=#=#= */
+
+    public function __construct(array $attributes = [])
+    {
+        parent::__construct($attributes);
+        $this->appends = array_merge(parent::getAppends(), [
+            //
+        ]);
+    }
+
+
+}
---
+++ database/migrations/2025_10_25_041445_create_exchange_rates_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use App\Traits\AuditColumnsTrait;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+     use AuditColumnsTrait;
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('exchange_rates', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('sort_order')->default(0);
+            $table->bigInteger('base_currency');
+            $table->bigInteger('target_currency');
+
+            $table->decimal('rate', 10, 6);
+            $table->timestamp('last_updated_at')->nullable();
+            
+            $table->foreign('base_currency')->references('id')->on('currencies')->cascadeOnDelete()->cascadeOnUpdate();
+            $table->foreign('target_currency')->references('id')->on('currencies')->cascadeOnDelete()->cascadeOnUpdate();
+
+            $table->softDeletes();
+            $table->timestamps();
+            $this->addAdminAuditColumns($table);
+        });
+    }
+    
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('exchange_rates');
+    }
+};
